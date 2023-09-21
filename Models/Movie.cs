b29c087b469b0e1f45fb63aad91c3c275425cb9d
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2023-09-21 15:33:31 -0600

    Search by genre and name

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,11 +5,15 @@ namespace RazorPagesMovie.Models;
 public class Movie
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
+
+    public string? Title { get; set; } = string.Empty;
+
     [DataType(DataType.Date)]
     [Display(Name = "Release Date")]
     public DateTime ReleaseDate { get; set; }
-    public string? Genre { get; set; }
+
+    public string? Genre { get; set; } = string.Empty;
+
     [Display(Name = "Precio!")]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
